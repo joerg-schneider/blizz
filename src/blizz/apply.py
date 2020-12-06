0f91732c7f613d1bbcea92a8ff07dbc71de2145a
Jörg Schneider <joerg@joerg-schneider.org>
2020-12-06 10:52:03 +0100

    add blizz.apply

---
+++ src/blizz/apply.py
@@ -0,0 +1,26 @@
+from blizz import _inspect
+import functools
+
+
+def deduplication(func):
+    @functools.wraps(func)
+    def _decorated(*args, **kwargs):
+        relation = _inspect.get_class_that_defined_method(func)
+        assert relation is not None
+        res = func(*args, **kwargs)
+        # _deduplication(r=relation, data=res) todo: implement this
+        return res
+
+    return _decorated
+
+
+def fill_defaults(func):
+    @functools.wraps(func)
+    def _decorated(*args, **kwargs):
+        relation = _inspect.get_class_that_defined_method(func)
+        assert relation is not None
+        res = func(*args, **kwargs)
+        # _fill_defaults(r=relation, data=res) todo: implement this
+        return res
+
+    return _decorated
